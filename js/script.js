7a64b178054e93dea1bba4c045414afb8cd22129
Darya Podskrebalina <2012dr2012@gmail.com>
2023-02-16 14:47:33 +0600

    fix: Fix translate weather error

--- js/script.js
+++ js/script.js
@@ -3,16 +3,14 @@ import "../css/style.css";
 
 import './modules/settings.js';
 import './modules/translate.js';
-
 import './modules/clock.js';
 import './modules/clock.js';
 import './modules/greeting.js';
 import './modules/image-slider.js';
-import  './modules/image-generator.js';
-
+import './modules/image-generator.js';
 import './modules/weather.js';
 import './modules/quote.js';
-import  './modules/playlist.js';
-import  './modules/audio-player.js';
+import './modules/playlist.js';
+import './modules/audio-player.js';
 
 
